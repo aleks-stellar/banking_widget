bbec6ffb1a1bba0224a5dd4fece8368514a44811
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-06-09 00:58:12 +0300

    test functions

--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,18 @@
 """
 В модуле реализованы две функции:
 Функция {mask_account_card}, которая умеет обрабатывать информацию как
-о картах, так и о счетах;
+о картах, так и о счетах.
 Функция {get_date}, которая принимает на вход строку с датой в формате
 '2024-03-11T02:26:18.671407' и возвращает строку с датой в формате
 'ДД.ММ.ГГГГ' ('11.03.2024').
 """
 
-from masks import get_mask_card_number
+from src import masks
 
 
 def mask_account_card(card_info: str) -> str:
-    """Функция принимает информацию о карте и маскирует ее номер"""
+    """Функция принимает информацию о карте и маскирует номер карты
+    или счета"""
     card_type_list = []
     card_info_list = card_info.split()
 
@@ -21,9 +22,22 @@ def mask_account_card(card_info: str) -> str:
     card_type = " ".join(card_type_list)
 
     card_number = int(card_info_list[-1])
-    return f"{card_type} {get_mask_card_number(card_number)}"
+
+    if card_type_list[0] == "Счет":
+        return f"{card_type} {masks.get_mask_account(card_number)}"
+    else:
+        return f"{card_type} {masks.get_mask_card_number(card_number)}"
+
+
+def get_date(date_str: str) -> str:
+    """Функция реформатирует дату"""
+    date_list = date_str.split("-", 2)
+    date_list[2] = date_list[2][:2]
+    date_list_reverse = ".".join(date_list[::-1])
+    return date_list_reverse
 
 
 if __name__ == "__main__":
-
-    print(mask_account_card("Visa Gold 5999414228426353"))
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(get_date("2024-03-11T02:26:18.671407"))
