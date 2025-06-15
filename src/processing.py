458f3c335b3885ac7cdb0b22fb5da9e198bf2923
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-06-15 20:18:27 +0300

    fix variable name

--- src/processing.py
+++ src/processing.py
@@ -2,44 +2,44 @@ from typing import Dict, List
 
 
 def filter_by_state(
-    dicts_list: List[Dict[str, str | int]],
-    state_key: str = "EXECUTED"
+    transactions_list: List[Dict[str, str | int]],
+    state: str = "EXECUTED"
 ) -> List[Dict[str, str | int]]:
     """
     Функция принимает список словарей и проводит его фильтрацию
     по ключу
     """
-    filtered_dicts_list = [
-        item_dict
-        for item_dict in dicts_list
-        if item_dict["state"] == state_key
+    filtered_transactions_list = [
+        transaction
+        for transaction in transactions_list
+        if transaction["state"] == state
     ]
-    return filtered_dicts_list
+    return filtered_transactions_list
 
 
 # Функция для сортировки даты
 def get_date_format(
-    dicts_list_date: Dict[str, str | int]
+    transaction_dict: Dict[str, str | int]
 ) -> int:
     """
     Функция принимает словарь и извлекает из
     него дату для дальнейшей сортировки
     """
-    date_str = str(dicts_list_date["date"])
+    date_str = str(transaction_dict["date"])
     date_list = date_str.split("-", 2)
     date_list[2] = date_list[2][:2]
-    date_list_reverse = "".join(date_list)
-    return int(date_list_reverse)
+    date_str_reverse = "".join(date_list)
+    return int(date_str_reverse)
 
 
 def sort_by_date(
-    dictionaries_list: List[Dict[str, str | int]],
+    unsorted_transactions_list: List[Dict[str, str | int]],
     sorting_direction: bool = True
 ) -> List[Dict[str, str | int]]:
     """
     Принимает на вход список словарей и сортирует его по дате
     """
-    sorted_dictionaries_list = sorted(
-        dictionaries_list, key=get_date_format, reverse=sorting_direction
+    sorted_transactions_list = sorted(
+        unsorted_transactions_list, key=get_date_format, reverse=sorting_direction
     )
-    return sorted_dictionaries_list
+    return sorted_transactions_list
