47ae7ed74b5c518d39fb0db7867e26ade706f1d1
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-06-29 15:59:18 +0300

    fix generators.py for linters

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,6 @@
 """Этот модуль содержит все новые функции,
 реализующие генераторы для обработки данных."""
 from typing import Iterator
-from random import randint
 
 
 def filter_by_currency(
@@ -15,107 +14,14 @@ def filter_by_currency(
             yield transaction
 
 
-# Пример использования функции
-transactions_list = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
-usd_transactions = filter_by_currency(transactions_list, "USD")
-
-for i in range(2):
-    print(next(usd_transactions))
-
-
 def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
     """Функция генерирует описание операций по очереди"""
     for transaction in transactions:
         yield transaction["description"]
 
 
-# Пример использования функции
-descriptions = transaction_descriptions(transactions_list)
-
-for i in range(5):
-    print(next(descriptions))
-
-
 def card_number_generator(
-        start: int=1, stop: int=9999999999999999
+        start: int = 1, stop: int = 9999999999999999
 ) -> Iterator[str]:
     """Функция генерирует номер карты в формате XXXX XXXX XXXX XXXX"""
     for num in range(start, stop + 1):
@@ -128,7 +34,3 @@ def card_number_generator(
             ]
         )
         yield card_number_str
-
-# Пример использования функции
-for card_number in card_number_generator(1, 5):
-    print(card_number)
