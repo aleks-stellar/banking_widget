382bb7e7cabe15e14fd7f80569887af9c8bda042
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-07-16 00:01:27 +0300

    create test for convert_currency

---
+++ tests/test_external_api.py
@@ -0,0 +1,35 @@
+from unittest.mock import patch, Mock
+from src.external_api import convert_currency
+
+
+@patch("src.external_api.requests.get")
+@patch("src.external_api.os.getenv")
+@patch("src.external_api.load_dotenv")
+def test_convert_currency(mock_load_dotenv, mock_getenv, mock_requests_get):
+    mock_load_dotenv.return_value = None
+
+    # Мок os.getenv для возврата фейкового ключа
+    mock_getenv.return_value = "fake_api_key"
+
+    # mock requests.get с фейковым JSON-ответом
+    mock_response = Mock()
+    mock_response.json.return_value = {"result": 123.45}
+    mock_requests_get.return_value = mock_response
+
+    # пример входных данных
+    operation = {
+        "operationAmount": {
+            "amount": 100,
+            "currency": {
+                "code": "USD"
+            }
+        }
+    }
+
+    # вызываем тестируемую функцию
+    result = convert_currency(operation)
+
+    assert result == 123.45
+    mock_load_dotenv.assert_called_once()
+    mock_getenv.assert_called_with("API_KEY_CONVERT")
+    mock_requests_get.assert_called_once()
