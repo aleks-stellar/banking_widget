bfed20bf29f555633d6ef49a5faf9fdb1f8f85ec
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-07-19 03:22:52 +0300

    create read_excel_transactions func

--- src/transactions_parser.py
+++ src/transactions_parser.py
@@ -1,6 +1,8 @@
 import csv
 from pathlib import Path
 
+import pandas as pd
+
 
 def read_csv_transactions(path_to_csv_file: Path) -> list[dict] | None:
     """
@@ -25,14 +27,67 @@ def read_csv_transactions(path_to_csv_file: Path) -> list[dict] | None:
                         "id": row["id"],
                         "state": row["state"],
                         "date": row["date"],
-                        "amount": row["amount"],
-                        "currency_name": row["currency_name"],
-                        "currency_code": row["currency_code"],
+                        "operationAmount": {
+                            "amount": row["amount"],
+                            "currency": {
+                                "name": row["currency_name"],
+                                "code": row["currency_code"]
+                            }
+                        },
+                        "description": row["description"],
                         "from": row["from"],
-                        "to": row["to"],
-                        "description": row["description"]
+                        "to": row["to"]
                     }
                 )
+
+        return result
+
+    except FileNotFoundError as e:
+        print(f"Ошибка: {e}")
+        return []
+
+    except KeyError as e:
+        print(f"Не найден ключ {e}")
+        return []
+
+    except Exception as e:
+        print(f"Непредвиденная ошибка: {e}")
+        return []
+
+
+def read_excel_transactions(path_to_excel_file: Path) -> list[dict] | None:
+    """
+    Считывает финансовые операции из EXCEL-файла
+    и преобразует их в список словарей.
+    :param path_to_excel_file: Путь к EXCEL-файлу с транзакциями.
+    :return: Список словарей с транзакциями.
+    """
+    result = []
+
+    try:
+        if not path_to_excel_file.exists():
+            raise FileNotFoundError(
+                f"Файл не найден по пути \"{path_to_excel_file}\""
+            )
+        excel_transactions = pd.read_excel(path_to_excel_file)
+        for _, row in excel_transactions.iterrows():
+            result.append(
+                {
+                    "id": row["id"],
+                    "state": row["state"],
+                    "date": row["date"],
+                    "operationAmount": {
+                        "amount": row["amount"],
+                        "currency": {
+                            "name": row["currency_name"],
+                            "code": row["currency_code"]
+                        }
+                    },
+                    "description": row["description"],
+                    "from": row["from"],
+                    "to": row["to"]
+                }
+            )
 
         return result
 
