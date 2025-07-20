0e82cccdaa067bc6001b2b780e246167676491bf
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-07-20 16:03:46 +0300

    fix test_transactions_parser.py

--- tests/test_transactions_parser.py
+++ tests/test_transactions_parser.py
@@ -18,103 +18,52 @@ INVALID_PATH_TO_FILE = Path(
 )
 
 
-@patch("src.transactions_parser.csv.DictReader")
-@patch("builtins.open")
+@patch("csv.DictReader")
 def test_read_csv_transactions_valid(
-        mock_open: Mock,
-        mock_dict_reader: Mock,
+        mock_file: Mock,
         rows_in_reader: Callable,
         opened_and_formatted_transactions: Callable
 ) -> None:
     """
-    Тест для проверки работы функции, принимающей корректные данные.
+    Тест для проверки работы функции read_csv_transactions,
+    принимающей корректные данные.
     """
-    mock_dict_reader.return_value = rows_in_reader
-    mock_file = Mock()
-    mock_open.return_value.__enter__.return_value = mock_file
+    mock_file.return_value = rows_in_reader
     result = read_csv_transactions(PATH_TO_FILE)
     assert result == opened_and_formatted_transactions
-    mock_open.assert_called_once_with(PATH_TO_FILE, encoding="utf-8")
-    mock_dict_reader.assert_called_once_with(mock_file, delimiter=";")
 
 
-@patch("src.transactions_parser.csv.DictReader")
-@patch("builtins.open")
-def test_read_csv_transactions_empty(
-        mock_open: Mock,
-        mock_dict_reader: Mock,
-) -> None:
-    """
-    Тест для проверки работы функции, принимающей пустой список транзакций.
-    """
-    mock_dict_reader.return_value = []
-    mock_file = Mock()
-    mock_open.return_value.__enter__.return_value = mock_file
-    result = read_csv_transactions(PATH_TO_FILE)
-    assert result == []
-    mock_open.assert_called_once_with(PATH_TO_FILE, encoding="utf-8")
-    mock_dict_reader.assert_called_once_with(mock_file, delimiter=";")
-
-
-@patch("src.transactions_parser.csv.DictReader")
-@patch("builtins.open")
-def test_read_csv_transactions_invalid_nesting(
-        mock_open: Mock,
-        mock_dict_reader: Mock,
-) -> None:
-    """
-    Тест для проверки работы функции, принимающей некорректные данные.
-    """
-    mock_dict_reader.return_value = [{"0ne": 1}, "Two", 2]
-    mock_file = Mock()
-    mock_open.return_value.__enter__.return_value = mock_file
-    result = read_csv_transactions(PATH_TO_FILE)
-    assert result == []
-    mock_open.assert_called_once_with(PATH_TO_FILE, encoding="utf-8")
-    mock_dict_reader.assert_called_once_with(mock_file, delimiter=";")
-
-
-@patch("src.transactions_parser.csv.DictReader")
-@patch("builtins.open")
-def test_read_csv_transactions_without_keys(
-        mock_open: Mock,
-        mock_dict_reader: Mock,
-) -> None:
-    """
-    Тест для проверки работы функции, принимающей
-    данные без необходимых ключей.
-    """
-    mock_dict_reader.return_value = [
-        {
-            "state": "EXECUTED",
-        },
-        {
-            "state": "EXECUTED",
-        }
-    ]
-    mock_file = Mock()
-    mock_open.return_value.__enter__.return_value = mock_file
-    result = read_csv_transactions(PATH_TO_FILE)
-    assert result == []
-    mock_open.assert_called_once_with(PATH_TO_FILE, encoding="utf-8")
-    mock_dict_reader.assert_called_once_with(mock_file, delimiter=";")
-
-
-@patch("src.transactions_parser.csv.DictReader")
-@patch("builtins.open")
 def test_read_csv_transactions_invalid_path(
-        mock_open: Mock,
-        mock_dict_reader: Mock,
-        rows_in_reader: Callable
+        rows_in_reader: Callable,
+        opened_and_formatted_transactions: Callable
 ) -> None:
     """
-    Тест для проверки работы функции, принимающей некорректный путь.
+    Тест для проверки работы функции read_csv_transactions,
+    принимающей некорректный путь.
     """
-    mock_dict_reader.return_value = rows_in_reader
-    mock_file = Mock()
-    mock_open.return_value.__enter__.return_value = mock_file
-    result = read_csv_transactions(INVALID_PATH_TO_FILE)
-    assert result == []
+    assert read_csv_transactions(INVALID_PATH_TO_FILE) == []
+
+
+@patch("csv.DictReader")
+def test_read_csv_transactions_without_keys(
+        mock_file: Mock
+) -> None:
+    """
+    Тест для проверки работы функции read_csv_transactions,
+    принимающей некорректные данные (без необходимых ключей).
+    """
+    mock_file.return_value = [{"0ne": 1}, {"Two", 2}]
+    assert read_csv_transactions(PATH_TO_FILE) == []
+
+
+@patch("csv.DictReader", side_effect=Exception)
+def test_read_csv_transactions_exception(
+        mock_file: Mock
+) -> None:
+    """
+    Тест проверяет обработку непредвиденных ошибок при чтении csv-файла.
+    """
+    assert read_csv_transactions(PATH_TO_FILE) == []
 
 
 @patch("pandas.read_excel")
@@ -124,7 +73,8 @@ def test_read_excel_transactions_valid(
         opened_and_formatted_transactions: Callable
 ) -> None:
     """
-    Тест для проверки работы функции, принимающей корректные данные.
+    Тест для проверки работы функции read_excel_transactions,
+    принимающей корректные данные.
     """
     mock_read_excel.return_value = df_data
     result = read_excel_transactions(PATH_TO_EXCEL_FILE)
@@ -139,7 +89,8 @@ def test_read_excel_transactions_invalid_path(
         opened_and_formatted_transactions: Callable
 ) -> None:
     """
-    Тест для проверки работы функции, принимающей некорректный путь.
+    Тест для проверки работы функции read_excel_transactions,
+    принимающей некорректный путь.
     """
     mock_read_excel.return_value = df_data
     result = read_excel_transactions(INVALID_PATH_TO_FILE)
@@ -151,7 +102,8 @@ def test_read_excel_transactions_missing_keys(
     mock_read_excel: Mock
 ) -> None:
     """
-    Тест для проверки работы функции, принимающей некорректный формат данных.
+    Тест для проверки работы функции read_excel_transactions,
+    принимающей некорректный формат данных.
     """
     mock_read_excel.return_value = pd.DataFrame(
         {
@@ -159,19 +111,6 @@ def test_read_excel_transactions_missing_keys(
             "state": ["EXECUTED"]
         }
     )
-    result = read_excel_transactions(PATH_TO_EXCEL_FILE)
-    assert result == []
-    mock_read_excel.assert_called_once_with(PATH_TO_EXCEL_FILE)
-
-
-@patch("pandas.read_excel")
-def test_read_excel_transactions_empty(
-    mock_read_excel: Mock
-) -> None:
-    """
-    Тест для проверки работы функции, принимающей пустой датафрейм.
-    """
-    mock_read_excel.return_value = pd.DataFrame()
     result = read_excel_transactions(PATH_TO_EXCEL_FILE)
     assert result == []
     mock_read_excel.assert_called_once_with(PATH_TO_EXCEL_FILE)
