87608ba44bf07150b838da5549b45aacd422c09b
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-07-23 02:20:58 +0300

    fix generators.py

--- src/generators.py
+++ src/generators.py
@@ -3,19 +3,33 @@
 from typing import Iterator
 
 
+# def filter_by_currency(
+#         transactions: list[dict],
+#         currency: str
+# ) -> Iterator[dict]:
+#     """Функция по списку транзакций возвращает итератор
+#     с транзакциями в определенной валюте"""
+#     usd_transactions_count = 0
+#     for transaction in transactions:
+#         if transaction["operationAmount"]["currency"]["code"] == currency:
+#             yield transaction
+#             usd_transactions_count += 1
+#         if usd_transactions_count == 0:
+#             raise ValueError("Не найдено транзакции в заданной валюте")
+
+
 def filter_by_currency(
         transactions: list[dict],
         currency: str
 ) -> Iterator[dict]:
     """Функция по списку транзакций возвращает итератор
     с транзакциями в определенной валюте"""
-    usd_transactions_count = 0
+
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
-            usd_transactions_count += 1
-        if usd_transactions_count == 0:
-            raise ValueError("Не найдено транзакции в заданной валюте")
+        else:
+            continue
 
 
 def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
